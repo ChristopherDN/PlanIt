7b81f10306aad173fc8e761f94fea74f02f61e79
jeppe-t <jeppethylstrup@yahoo.dk>
2021-12-01 12:27:06 +0100

    TaskController code clean-up + comments on endpoints.

--- src/main/java/now/planit/Controllers/TaskController.java
+++ src/main/java/now/planit/Controllers/TaskController.java
@@ -9,18 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
-
 import java.util.ArrayList;
-
-
 
 @Controller
 public class TaskController {
   User user;
+  String projectName;
   TaskService taskService = new TaskService();
   ArrayList<Task> tasks = new ArrayList();
-  String projectName;
 
+  //Endpoint to dynamic display(loop) all task and display user information
   @GetMapping("/createTask")
   public String createTasks(WebRequest request, Model model) {
     user = (User) request.getAttribute("user", WebRequest.SCOPE_SESSION);
@@ -30,26 +28,27 @@ public class TaskController {
     return "/project/createTask";
   }
 
+  //Endpoint to pass and connect data(id) from myProjects site to createtask site.
   @GetMapping("/update/{id}")
   public String updateProject(@PathVariable(value = "id") String id, WebRequest request, Model model) {
-    user = (User) request.getAttribute("user", WebRequest.SCOPE_SESSION);
-    System.out.println("get" + user);
     projectName = id;
+    user = (User) request.getAttribute("user", WebRequest.SCOPE_SESSION);
     model.addAttribute("tasks", tasks);
-    model.addAttribute("userName", user.getName());
     tasks = taskService.getTasks(id, user);
     return "redirect:/createTask";
   }
 
+  //Endpoint that stores parameters from task and pass them down to the service.
   @PostMapping("/createTaskParam")
   public String createTask(WebRequest request, Model model) {
-    System.out.println("post" + user);
     user = (User) request.getAttribute("user", WebRequest.SCOPE_SESSION);
     model.addAttribute("userName", user.getName());
-    taskService.createTask(request.getParameter("taskName"),
+    taskService.createTask
+            (request.getParameter("taskName"),
             request.getParameter("startDate"),
             request.getParameter("finishDate"),
-            Integer.parseInt(request.getParameter("cost")), projectName, user);
+            Integer.parseInt(request.getParameter("cost")),
+            projectName, user);
     return "redirect:/createTask";
   }
 
