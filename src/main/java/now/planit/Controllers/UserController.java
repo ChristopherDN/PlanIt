a0f17a43744bc384fb268fbe93b5b584ad36d936
jeppe-t <jeppethylstrup@yahoo.dk>
2021-12-06 15:42:51 +0100

    ...

--- src/main/java/now/planit/Controllers/UserController.java
+++ src/main/java/now/planit/Controllers/UserController.java
@@ -31,9 +31,12 @@ public class UserController {
 
   @PostMapping("/register")
   public String register(WebRequest request)throws UserNotExistException {
+
+     //TODO skal rettes til, så man kan logge ind uden nogen bruger i forvejen
     if(user == null){
       throw new UserNotExistException("Sorry, the username was allready taken. Please create a user with another name");
     }
+
     userService.registerUser(
             request.getParameter("name"),
             request.getParameter("email"),
