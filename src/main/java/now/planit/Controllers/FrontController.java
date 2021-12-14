b192f234063f8500988e104c55b6c4b2a088428a
Roed <Geofery10@hotmail.com>
2021-12-14 10:35:43 +0100

    rollback to buildable version

--- src/main/java/now/planit/Controllers/FrontController.java
+++ src/main/java/now/planit/Controllers/FrontController.java
@@ -1,9 +1,12 @@
 package now.planit.Controllers;
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpSession;
 
-@org.springframework.stereotype.Controller
+//@org.springframework.stereotype.Controller
+@Controller
 public class FrontController {
 
 
